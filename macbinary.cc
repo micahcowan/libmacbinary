ff204966f4c1b7fab4daf3ee67c93273fba00c69
Micah Cowan <micah@addictivecode.org>
2021-08-28 15:08:48 -0700

    Find the start of the resource fork.

--- macbinary.cc
+++ macbinary.cc
@@ -1,7 +1,19 @@
 #include "macbinary.hh"
 
+using std::uint16_t;
+using std::size_t;
+
 MacBinary::ResourceFork & MacBinary::getResourceFork()
 {
-    if (!_res) _res = new ResourceFork();
+    const unsigned char *resStart;
+    std::size_t resLen;
+
+    // XXX confirm we don't run past _len
+    resStart = _data + 128u + _getU16(83u);
+    resLen   = _getU16(87u);
+    // XXX ensure multiples of 128
+
+    if (!_res) _res = new ResourceFork(*this, resStart, resLen);
     return *_res;
 }
+
--- macbinary.hh
+++ macbinary.hh
@@ -2,20 +2,22 @@
 #define MACBINARY_HH
 
 #include <cstdlib>
+#include <cstdint>
 
 class MacBinary {
     public:
-        class ResourceFork {
-            friend class MacBinary;
-            protected:
-                ResourceFork() {}
-        };
+        class ResourceFork;
 
     private:
         const unsigned char * const _data;
         const std::size_t _len;
         /* XXX this should be an autoptr (or whatever modern equiv) */
         ResourceFork *_res = NULL;
+
+    protected:
+        std::uint16_t _getU16(std::size_t loc) {
+            return _data[loc] * 256 + _data[loc + 1];
+        }
 
     public:
         MacBinary(const unsigned char *p, std::size_t len)
@@ -33,6 +35,22 @@ class MacBinary {
         virtual ResourceFork &getResourceFork();
 
         virtual ~MacBinary() {}
+};
+
+class MacBinary::ResourceFork {
+    friend class MacBinary;
+    private:
+        MacBinary                 & _mb;
+        const unsigned char * const _data;
+        const std::size_t           _len;
+    protected:
+        ResourceFork(MacBinary &mb, const unsigned char *p, std::size_t len)
+            : _mb(mb), _data(p), _len(len)
+            {}
+    public:
+        virtual ~ResourceFork() {}
+
+
 };
 
 #endif // MACBINARY_HH
