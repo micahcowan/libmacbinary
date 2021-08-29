a5cd1a9ac6d6d3e08efefac9acf397126bd96f69
Micah Cowan <micah@addictivecode.org>
2021-08-29 10:41:13 -0700

    main: Accept file names as args

--- main.cc
+++ main.cc
@@ -1,29 +1,68 @@
 #include <iostream>
+#include <cstring>
+#include <cerrno>
+#include <cstdlib>
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 
 #include "macbinary.hh"
 #include "ressectioniter.hh"
 
 using namespace libmacbinary;
 
+using std::cerr;
 using std::cout;
 using std::endl;
 using std::hex;
 using std::dec;
+using std::size_t;
+using std::string;
 
-int main(void) {
-    size_t sz = 40320;
-    int fd = open("fbapp.bin", O_RDONLY);
+void printError(const string &msg)
+{
+    cerr << msg << ": " << strerror(errno) << endl;
+}
+
+unsigned char *mmapFile(const char * const fileName, size_t *sz)
+{
+    unsigned char *mem = NULL;
+
+    // Open the file
+    errno = 0;
+    int fd = open(fileName, O_RDONLY);
     if (fd == -1) {
-        cout << "Die." << endl;
-        return 1;
+        printError("Couldn't open file");
+        return NULL;
     }
-    unsigned char *mem = (unsigned char *)mmap(NULL, sz, PROT_READ, MAP_PRIVATE, fd, 0);
-    close(fd);
 
+    // Get the file's size for mmap
+    errno = 0;
+    struct stat buf;
+    int result = fstat(fd, &buf);
+    if (result != 0) {
+        printError("Couldn't stat file (so no mmap)");
+        goto close_and_end;
+    }
+    *sz = buf.st_size;
+
+    // Mmap the file
+    errno = 0;
+    mem = static_cast<unsigned char *>(mmap(NULL, *sz, PROT_READ, MAP_PRIVATE, fd, 0));
+    if (mem == NULL) {
+        printError("Couldn't mmap file");
+        goto close_and_end;
+    }
+
+close_and_end:
+    close(fd);
+    return mem;
+}
+
+void printResFork(unsigned char * mem, size_t sz)
+{
     cout << "Start of file in memory: " << (void*)mem << endl;
 
     MacBinary mb(mem, sz);
@@ -37,4 +76,29 @@ int main(void) {
     for (auto i = rf.getSections(); i != end; ++i) {
         cout << "  " << *i << endl;
     }
+    cout << endl;
+}
+
+int main(int argc, char **argv) {
+    unsigned char *mem;
+    size_t sz;
+    int status = 0;
+
+    if (*argv != NULL) ++argv;   // Skip our program's name
+
+    while (*argv != NULL) {
+        cout << *argv << ":" << '\n' << endl;
+        mem = mmapFile(*argv, &sz);
+        if (mem) {
+            printResFork(mem, sz);
+            munmap(static_cast<void*>(mem), sz);
+        }
+        else {
+            status = 1;
+        }
+
+        ++argv;
+    }
+
+    return status;
 }
